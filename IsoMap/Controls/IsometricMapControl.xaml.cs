8d768076237097ff4c41fac0d21abb715f8ff8be
Robert Schumacher <ras0219@outlook.com>
2016-03-06 12:01:39 -0800

    Fixed movement out of rect and stomping on others

--- IsoMap/Controls/IsometricMapControl.xaml.cs
+++ IsoMap/Controls/IsometricMapControl.xaml.cs
@@ -87,6 +87,10 @@ namespace IsoMap.Controls
             var pos2 = pos - TerrainTopLeft;
             return new IntVector2((int)pos2.X, (int)pos2.Y);
         }
+        private Vector2 TerrainXYToWorld(IntVector2 pos)
+        {
+            return new Vector2(pos.X, pos.Y) + TerrainTopLeft;
+        }
         private bool ValidTerrainXY(IntVector2 v) { return ValidTerrainXY(v.X, v.Y); }
         private bool ValidTerrainXY(int x, int y) { return (x >= 0 && x < TerrainSize.X) && (y >= 0 && y < TerrainSize.Y); }
 
@@ -183,7 +187,8 @@ namespace IsoMap.Controls
                             for (var x = -1; x < 2; ++x)
                             {
                                 var tpos2 = tpos + new IntVector2(x, y);
-                                if (ValidTerrainXY(tpos2))
+                                var wpos2 = TerrainXYToWorld(tpos2);
+                                if (ValidTerrainXY(tpos2) && !TeamA.Contains(wpos2) && !TeamB.Contains(wpos2))
                                     MovableOverlay.Set(TerrainXYToIndex(tpos2), true);
                             }
                         }
@@ -194,7 +199,8 @@ namespace IsoMap.Controls
             {
                 if (ActivePhase == Phase.Move)
                 {
-                    if (SelectedTile != null && Vector2.Distance(SelectedTile.Value, selectedTile) <= 1.44f)
+                    var terxy = WorldToTerrainXY(selectedTile);
+                    if (SelectedTile != null && ValidTerrainXY(terxy) && MovableOverlay.Get(TerrainXYToIndex(terxy)))
                     {
                         if (TeamA.Contains(SelectedTile.Value))
                         {
